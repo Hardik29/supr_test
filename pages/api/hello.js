516dd0d8f8a5a56c79d429fab24203de8d4456b6
Hardik29 <singhhardik14@gmail.com>
2024-03-21 13:44:27 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import crypto from "crypto";
+
+function hmac_rawurlsafe_base64_string(distinct_id, secret) {
+  const hash = crypto
+    .createHmac("sha256", secret)
+    .update(distinct_id)
+    .digest("base64url");
+  return hash.trimEnd("=");
+}
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const id = hmac_rawurlsafe_base64_string( "hg@gmail.com", "KUQPHkf5Sfyx5em5fJSRqo0T5nuiOAGG6vq8FUKowfo")
+  res.status(200).json({ id: id });
 }
